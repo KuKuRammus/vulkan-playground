d0a3ede4aa36092adb4122f5089f6dc03cd00aba
KuKuRammus <vladkamyshanov@gmail.com>
2020-06-01 20:57:02 +0300

    Add pipeline creation

--- src/main.c
+++ src/main.c
@@ -60,6 +60,9 @@ VkImageView* vulkanSwapChainImageViews;
 // Rendering surface
 VkSurfaceKHR vulkanSurface;
 
+// Rendering pipeline
+VkPipelineLayout vulkanPipelineLayout;
+
 typedef struct {
     u8* data;
     i64 size;
@@ -782,24 +785,149 @@ void createGraphicsPipeline() {
 
     printf("Assigning shaders to pipeline states\n");
     // Vertex shader
-    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
-    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
-    vertShaderStageInfo.module = vertShaderModule;
-    vertShaderStageInfo.pName = "main";
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .stage = VK_SHADER_STAGE_VERTEX_BIT,
+        .module = vertShaderModule,
+        .pName = "main"
+    };
 
     // Fragment shader
-    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
-    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-    fragShaderStageInfo.module = fragShaderModule;
-    fragShaderStageInfo.pName = "main";
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
+        .module = fragShaderModule,
+        .pName = "main"
+    };
 
     // Declare pipeline as an array
     VkPipelineShaderStageCreateInfo shaderStages[] = {
         vertShaderStageInfo,
         fragShaderStageInfo
     };
+
+    // Describe vertex data format
+    // Because now data is hardcoded into shader, don't pass data to it
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .vertexBindingDescriptionCount = 0,
+        .pVertexBindingDescriptions = NULL,
+        .vertexAttributeDescriptionCount = 0,
+        .pVertexAttributeDescriptions = NULL
+    };
+
+    // Describe what kind of geometry to draw from provided vertices
+    // and if primitive restart should be enabled
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+        .primitiveRestartEnable = VK_FALSE
+    };
+
+    // Define viewport to render to
+    VkViewport viewport = {
+        .x = 0.0f,
+        .y = 0.0f,
+        .width = (f32)vulkanSwapChainExtent.width,
+        .height = (f32)vulkanSwapChainExtent.height,
+        .minDepth = 0.0f,
+        .maxDepth = 1.0f
+    };
+
+    // Define range, outside of which every geometry will be discarded
+    VkRect2D scissor = {
+        .offset = {0, 0},
+        .extent = vulkanSwapChainExtent
+    };
+
+    // Combine viewport and scissor into viewport state
+    VkPipelineViewportStateCreateInfo viewportState = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+        .viewportCount = 1,
+        .pViewports = &viewport,
+        .scissorCount = 1,
+        .pScissors = &scissor
+    };
+
+    // Define rasterizer
+    // (thing which will turn geometry into fragments to be colored by frag shader)
+    VkPipelineRasterizationStateCreateInfo rasterizer = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .depthClampEnable = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .lineWidth = 1.0f,
+        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .frontFace = VK_FRONT_FACE_CLOCKWISE,
+        .depthBiasEnable = VK_FALSE,
+        .depthBiasConstantFactor = 0.0f,
+        .depthBiasClamp = 0.0f,
+        .depthBiasSlopeFactor = 0.0f
+    };
+
+    // Define multisampling
+    // used for antialiasing
+    VkPipelineMultisampleStateCreateInfo multisampling = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .sampleShadingEnable = VK_FALSE,
+        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+        .minSampleShading = 1.0f,
+        .pSampleMask = NULL,
+        .alphaToCoverageEnable = VK_FALSE,
+        .alphaToOneEnable = VK_FALSE
+    };
+
+    // Define depth testing and stencil
+    // TODO: this is skipped for now
+    
+    // Define how resulting colors should be blended with existing colors
+    VkPipelineColorBlendAttachmentState colorBlendAttachment = {
+        .colorWriteMask = (
+            VK_COLOR_COMPONENT_R_BIT
+            | VK_COLOR_COMPONENT_G_BIT
+            | VK_COLOR_COMPONENT_B_BIT
+            | VK_COLOR_COMPONENT_A_BIT
+        ),
+        .blendEnable = VK_FALSE,
+        .srcColorBlendFactor = VK_BLEND_FACTOR_ONE,
+        .dstColorBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .colorBlendOp = VK_BLEND_OP_ADD,
+        .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
+        .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .alphaBlendOp = VK_BLEND_OP_ADD
+    };
+
+    // Define blend constants
+    VkPipelineColorBlendStateCreateInfo colorBlending = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .logicOpEnable = VK_FALSE,
+        .logicOp = VK_LOGIC_OP_COPY,
+        .attachmentCount = 1,
+        .pAttachments = &colorBlendAttachment,
+        .blendConstants = {0.0f, 0.0f, 0.0f, 0.0f}
+    };
+
+    // Define dynamic component of pipeline
+    // TODO: this is skipped for now
+
+    // Create pipeline layout
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .setLayoutCount = 0,
+        .pSetLayouts = NULL,
+        .pushConstantRangeCount = 0,
+        .pPushConstantRanges = NULL
+    };
+
+    VkResult result = vkCreatePipelineLayout(
+        vulkanDevice,
+        &pipelineLayoutInfo,
+        NULL,
+        &vulkanPipelineLayout
+    );
+    if (result != VK_SUCCESS) {
+        printf("[ERROR] Failed to create pipeline layout\n");
+    }
 
     // Destroy modules
     vkDestroyShaderModule(vulkanDevice, fragShaderModule, NULL);
@@ -824,6 +952,9 @@ void initVulkan() {
 }
 
 void shutdownVulkan() {
+    printf("Shutting down pipeline\n");
+    vkDestroyPipelineLayout(vulkanDevice, vulkanPipelineLayout, NULL);
+
     printf("Destroying image views\n");
     for (u32 i = 0; i < vulkanSwapChainImageCount; i++) {
         vkDestroyImageView(vulkanDevice, vulkanSwapChainImageViews[i], NULL);
